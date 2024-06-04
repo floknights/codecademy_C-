80e2b69af8bf1ce37924ad0b840afbdfe89cd1f9
Flo <123563998+floknights@users.noreply.github.com>
2024-06-05 00:43:02 +0100

    Create rock_paper_scissors.cpp

---
+++ rock_paper_scissors.cpp
@@ -0,0 +1,35 @@
+/*
+Rock Paper Scissors Lizard Spock
+(The Big Bang Theory)
+*/
+
+#include <iostream>
+#include <stdlib.h>
+
+int main() {
+
+  // Live long and prosper
+  
+  srand (time(NULL));
+
+  int computer = rand() % 3 + 1;
+
+  int user = 0;
+
+  std::cout << rand() % 3 + 1 << "\n";
+  std::cout << rand() % 3 + 1 << "\n";
+  std::cout << rand() % 3 + 1 << "\n";
+
+  std::cout << "====================\n";
+  std::cout << "rock paper scissors!\n";
+  std::cout << "====================\n";
+
+  std::cout << "1) ✊\n";
+  std::cout << "2) ✋\n";
+  std::cout << "3) ✌️\n";
+
+  std::cout << "shoot! ";
+
+  std::cin >> user;
+
+}
